b0cf187b80e72e89bfa33d2ac242158c0f736614
daviddpi <piscopodavid97@gmail.com>
2021-08-26 15:21:23 +0200

    esercizio completato

---
+++ js/myscript.js
@@ -0,0 +1,28 @@
+/*
+*   1 - chiedi all’utente il cognome
+*   2 - inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+*   3 - stampa la lista ordinata alfabeticamente
+*   4 - scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+*/
+
+//punto numero 1 fatto, chiesto cognome all'utente
+let listaCognomi = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+let mioCognome = prompt("Inserisci il tuo cognome");
+
+//rendo maiuscola la prima lettera del cognome
+if(mioCognome != (mioCognome.charAt(0).toUpperCase() + mioCognome.slice(1)) ){
+    mioCognome = mioCognome.charAt(0).toUpperCase() + mioCognome.slice(1);
+}
+
+//inserisco il cognome nell'array (nella lista degli altri cognomi)
+listaCognomi.push(mioCognome);
+
+//sort la funziona che ordina alfabeticamente la lista
+listaCognomi.sort();
+
+//funzione che indica la posizione dell'elemento aggiunto, +1 poichè gli array partono da 0
+let posizione = (listaCognomi.indexOf(mioCognome)) + 1;
+
+//stampo la lista e la posizione
+console.log(listaCognomi);
+console.log(posizione);
